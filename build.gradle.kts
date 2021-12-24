e11345fbd750fbcda42a317a008e38309272fd86
Tyler Cook <tcc@sandpolis.com>
2021-12-24 16:57:07 -0600

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+//============================================================================//
+//                                                                            //
+//            Copyright © 2015 - 2022 Sandpolis Software Foundation           //
+//                                                                            //
+//  This source file is subject to the terms of the Mozilla Public License    //
+//  version 2. You may not use this file except in compliance with the MPLv2. //
+//                                                                            //
+//============================================================================//
+
+plugins {
+	id("java-library")
+	id("org.s7s.build.module")
+	id("org.s7s.build.protobuf")
+	id("org.s7s.build.publish")
+}
+
+dependencies {
+	testImplementation("org.junit.jupiter:junit-jupiter-api:5.+")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.+")
+
+	if (project.getParent() == null) {
+		implementation("org.s7s:core.foundation:+")
+	} else {
+		implementation(project(":core:org.s7s.core.foundation"))
+	}
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+//============================================================================//
+//                                                                            //
+//            Copyright © 2015 - 2022 Sandpolis Software Foundation           //
+//                                                                            //
+//  This source file is subject to the terms of the Mozilla Public License    //
+//  version 2. You may not use this file except in compliance with the MPLv2. //
+//                                                                            //
+//============================================================================//
+
+rootProject.name = "org.s7s.core.protocol"
+
+buildscript {
+	repositories {
+		maven {
+			url = uri("https://plugins.gradle.org/m2/")
+		}
+	}
+	dependencies {
+		classpath("org.s7s:org.s7s.build:+")
+	}
+}
